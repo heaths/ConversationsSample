777f45f98fb875655e33553d101f9f8af2d2e0e7
Heath Stewart <heaths@microsoft.com>
2022-11-28 16:11:58 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Azure.AI.Language.Conversations;
+using Azure.AI.TextAnalytics;
+using Azure.Core;
+using Azure.Identity;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+
+var command = GetCommand();
+command.Handler = CommandHandler.Create<Options>(async options =>
+{
+    DefaultAzureCredential credential = new();
+    ConversationAnalysisClient conversationClient = new(options.Endpoint, credential);
+
+    var request = new
+    {
+        analysisInput = new
+        {
+            conversationItem = new
+            {
+                text = options.Question,
+                id = "1",
+                participantId = "1",
+            }
+        },
+        parameters = new
+        {
+            projectName = options.Project,
+            deploymentName = options.Deployment,
+            stringIndexType = "Utf16CodeUnit",
+        },
+        kind = "Conversation",
+    };
+
+    var response = await conversationClient.AnalyzeConversationAsync(RequestContent.Create(request));
+    dynamic json = JsonData.FromStream(response.ContentStream);
+
+    Console.WriteLine("Conversation Analysis recognized:");
+    foreach (var entity in json.result.prediction.entities)
+    {
+        Console.WriteLine($"{entity.category}: {entity.text}");
+    }
+    Console.WriteLine();
+
+    TextAnalyticsClient textAnalyticsClient = new(options.Endpoint, credential);
+    CategorizedEntityCollection entities = await textAnalyticsClient.RecognizeEntitiesAsync(options.Question);
+
+    Console.WriteLine("Text Analytics recognized:");
+    foreach (var entity in entities)
+    {
+        Console.WriteLine($"{entity.Category}: {entity.Text}");
+    }
+    Console.WriteLine();
+});
+await command.InvokeAsync(args);
+
+#region Helpers
+static Command GetCommand() => new RootCommand("Cognitive Services - Language Sample")
+{
+    new Option<Uri>("--endpoint", getDefaultValue: () => Uri.TryCreate(Environment.GetEnvironmentVariable("CONVERSATIONS_ENDPOINT"), UriKind.Absolute, out var endpoint) ? endpoint : null) { IsRequired = true },
+    new Option<string>("--project", getDefaultValue: () => Environment.GetEnvironmentVariable("CONVERSATIONS_PROJECT_NAME")) { IsRequired = true },
+    new Option<string>("--deployment", getDefaultValue: () => Environment.GetEnvironmentVariable("CONVERSATIONS_DEPLOYMENT_NAME") ?? "production") { IsRequired = true },
+    new Option<string>(new[] {"--question", "-q" }, getDefaultValue: () => "Send an email to Carol about tomorrow's demo") { IsRequired = true },
+};
+
+class Options
+{
+    public Uri Endpoint { get; set; }
+    public string Project { get; set; }
+    public string Deployment { get; set; }
+    public string Question { get; set; }
+}
+#endregion
